4ce3dd6e8ecbf7bcd67d18616c78370f4548bd4d
victorzarzar <victorzarzar58@gmail.com>
2024-01-11 16:54:04 -0300

    add function openNoteBox

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key, required String title});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  void openNoteBox() {
+    showDialog(
+        context: context,
+        builder: (context) => const AlertDialog(content: TextField()));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Notes")),
-      floatingActionButton: FloatingActionButton(onPressed: () {}),
+      floatingActionButton: FloatingActionButton(
+          onPressed: openNoteBox, child: const Icon(Icons.add)),
     );
   }
 }
